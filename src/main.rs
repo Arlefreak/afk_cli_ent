22a3874ae442a4e7b13e01bef3827c6e0cbb9a78
Arlefreak <hi@arlefreak.com>
2016-12-08 00:08:33 -0600

    Initial project printing;

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ extern crate rustc_serialize;
 mod afk;
 
 use clap::{Arg, App};
+use termion::{clear, color, cursor};
+use termion::color::Fg;
+use termion::input::TermRead;
+use std::fmt::Display;
+use std::io::{Write, stdout, stdin};
+use std::{time, thread};
+
+const SPARKS:&'static str="_▁▁▂▃▄▄▅▆▇██";
 
 fn main(){
     let matches = App::new("afk-ent")
@@ -29,6 +37,16 @@ fn main(){
     let config = matches.value_of("config").unwrap_or("default.conf");
     println!("Value for config: {}", config);
 
+    test_afk();
+    test_colors();
+    println!("");
+    test();
+    println!("");
+    print_projects();
+    // read_passwd();
+}
+
+fn test_afk(){
     afk::afk();
     println!("{:?}", afk::about::get_entry(2).slug);
     println!("{:?}", afk::about::get_entries()[0].slug);
@@ -38,4 +56,68 @@ fn main(){
     println!("{:?}", afk::portfolio::get_projects()[0].slug);
     println!("{:?}", afk::portfolio::get_project_category(1).slug);
     println!("{:?}", afk::portfolio::get_project_categories()[0].slug);
+}
+
+fn format_cap<T:Display>(content:T, capacity:i32) -> String{
+    match capacity{
+        0...5    => format!("{1}{0}", content, Fg(color::Red)),
+        5...10   => format!("{1}{0}", content, Fg(color::Red)),
+        10...20  |
+            20...30  => format!("{1}{0}", content, Fg(color::LightRed)),
+        30...40  => format!("{1}{0}", content, Fg(color::Yellow)),
+        40...55  => format!("{1}{0}", content, Fg(color::LightYellow)),
+        45...65  => format!("{1}{0}", content, Fg(color::Green)),
+        65...100 => format!("{1}{0}", content, Fg(color::LightGreen)),
+        _        => format!("{1}{0}", content, Fg(color::Black))
+    }
+}
+
+fn test(){
+    for (i,c) in SPARKS.chars().enumerate() {
+        let bar = format_cap(c, (i as i32)*10);
+        print!("{bar}", bar = &bar);
+    }
+}
+
+fn test_colors() {
+    for cap in 1..20{
+        let capacity = cap * 5;
+        let bar = SPARKS.chars().nth(10).unwrap_or('x');
+        let bar = format_cap(bar, capacity);
+        print!("{bar}", bar = &bar);
+    }
+}
+
+// fn read_passwd(){
+//     let stdout = stdout();
+//     let mut stdout = stdout.lock();
+//     let stdin = stdin();
+//     let mut stdin = stdin.lock();
+
+//     stdout.write(b"password: ").unwrap();
+//     stdout.flush().unwrap();
+
+//     let pass = stdin.read_passwd(&mut stdout);
+//     print!("hola");
+
+//     if let Ok(Some(pass)) = pass {
+//         stdout.write(pass.as_bytes()).unwrap();
+//         stdout.write(b"\n").unwrap();
+//     } else {
+//         stdout.write(b"Error\n").unwrap();
+//     }
+// }
+
+fn print_projects(){
+    loop{
+        let projects: Vec<afk::portfolio::Project> = afk::portfolio::get_projects();
+        println!("\n{}{}{}{}{}{}", cursor::Hide, clear::All, cursor::Goto(1, 1), color::Fg(color::White), color::Bg(color::Black), projects[0].slug);
+        println!("\n{}{}{}", cursor::Hide, clear::All, color::Bg(color::Black));
+        for (index, project) in projects.iter().enumerate() {
+            println!("\n{}{}{}{}{}.- {}", cursor::Hide, cursor::Goto(1, (index as u16)+1), color::Fg(color::White), color::Bg(color::Black), index,project.slug);
+        }
+        println!("{}{}{}           Arlefreak.com           ", cursor::Goto(1, 1), color::Fg(color::Black),color::Bg(color::White));
+        println!("{}{}{}           Projects                ", cursor::Goto(1, 28), color::Fg(color::Black),color::Bg(color::White));
+        thread::sleep(time::Duration::from_millis(900));
+    }
 }
