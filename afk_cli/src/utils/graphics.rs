b54e7cfbdbd7e570c82c8a727e8db82fe69d7624
Arlefreak <hi@arlefreak.com>
2016-12-09 19:46:45 -0600

    Added graphics

---
+++ afk_cli/src/utils/graphics.rs
@@ -0,0 +1,8 @@
+pub const TOP_LEFT_CORNER: &'static str = "╔";
+pub const TOP_RIGHT_CORNER: &'static str = "╗";
+pub const BOTTOM_LEFT_CORNER: &'static str = "╚";
+pub const BOTTOM_RIGHT_CORNER: &'static str = "╝";
+pub const VERTICAL_WALL: &'static str = "║";
+pub const HORIZONTAL_WALL: &'static str = "═";
+pub const VERTICAL_SNAKE_BODY: &'static str = "║";
+pub const HORIZONTAL_SNAKE_BODY: &'static str = "═";
